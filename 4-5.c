42bdb83dde033b26bccc69afaf2e15fe517d5404
Le-coeur <44370812+Le-coeur@users.noreply.github.com>
2020-10-24 22:58:14 +0800

    Add files via upload

---
+++ 4-5.c
@@ -0,0 +1,7 @@
+#include"4-1.h"
+int main(){
+    int d1;
+    d1=open("./out.info", O_CREAT|O_TRUNC|O_WRONLY,0644);
+    dup2(d1,1);
+    printf("Redirect stdout file!\n");
+    return 0;
---
+++ 4-7.c
@@ -0,0 +1,9 @@
+#include"4-1.h"
+int main(){
+    FILE *fp;
+    int fd;
+    fp=fopen("./ch03.h", "r");
+    fd=fileno(fp);
+    printf("fd=%d\n",fd);
+    return 0;
+}
